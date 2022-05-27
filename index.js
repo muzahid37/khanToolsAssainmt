be0aa2fe9560695192b0b83f1501b1355bb581a0
muzahid37 <muzahidul372709@gmail.com>
2022-05-27 08:11:19 +0600

    added for check hero error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3200;
 
 app.use(cors());
 app.use(express.json());
